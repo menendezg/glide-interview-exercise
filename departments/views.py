218e593f78eae5935675185a3cfe6e0ec6b6ebd1
Gabriel Menendez <gabriel.menendez@mediamonks.com>
2020-03-13 14:41:20 -0300

    Add class representation for office and department

--- departments/views.py
+++ departments/views.py
@@ -1,12 +1,28 @@
-from flask import abort, jsonify
+from flask import (abort, jsonify, request)
 from glide_api.app import app
 from glide_api.utils import json_reader
 
 departments = json_reader(file="departments.json", folder="departments/data")
 
 
+class Department:
+    @staticmethod
+    def get_departments(limit=100, offset=0):
+        """
+        Get departments with limit and offset
+        :param limit: max number of records returned
+        :param offset: index at wich to start
+        :return list of departments
+        """
+        return departments[offset: (offset + limit)]
+
+
 @app.route("/departments")
 def get_departments():
+    offset = int(request.args.get("offset", "0"))
+    limit = int(request.args.get("limit", "100"))
+    department = Department()
+    return jsonify({"offices": department.get_departments(limit, offset)})
     return jsonify({"departments": departments})
 
 
--- offices/views.py
+++ offices/views.py
@@ -1,13 +1,23 @@
-from flask import abort, jsonify
+from flask import abort, jsonify, request
 from glide_api.app import app
 from glide_api.utils import json_reader
 
+# we load data in memory
 offices = json_reader(file="offices.json", folder="offices/data")
+
+
+class Office:
+    @staticmethod
+    def get_offices(limit=100, offset=0):
+        return offices[offset: (offset + limit)]
 
 
 @app.route("/offices", methods=["GET"])
 def get_offices():
-    return jsonify({"offices": offices})
+    offset = int(request.args.get("offset", "0"))
+    limit = int(request.args.get("limit", "100"))
+    office = Office()
+    return jsonify({"offices": office.get_offices(limit, offset)})
 
 
 @app.route("/offices/<int:office_id>", methods=["GET"])
