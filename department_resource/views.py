f9927540924e3127133093e6ac5b111d813ebbef
Gabriel Menendez <gabriel.menendez@mediamonks.com>
2020-03-30 11:42:10 -0300

    Add check expand word

--- department_resource/views.py
+++ department_resource/views.py
@@ -5,11 +5,28 @@ from glide_api.resources.businessresource import BusinessResource
 from glide_api.utils import get_keywords
 
 
+@app.errorhandler(400)
+def custom400(error):
+    response = jsonify({'message': error.description['message']})
+
+
+def invalid_keyword(key_words):
+    valid_keys = ['superdepartment']
+    for keys in key_words:
+        for key in keys:
+            if key in valid_keys:
+                continue
+            else:
+                return True
+
+
 @app.route('/departments', methods=['GET'])
 def get_departments():
     offset = int(request.args.get('offset', '0'))
     limit = int(request.args.get('limit', '100'))
     key_words = get_keywords(request.args.getlist("expand", None))
+    if invalid_keyword(key_words):
+        abort(404, 'check your keywords to expand')
     handler = BusinessResource()
     departments = handler.get_all(Department, offset, limit, key_words)
     return jsonify(departments)
@@ -18,6 +35,8 @@ def get_departments():
 @app.route('/departments/<int:department_id>', methods=['GET'])
 def get_department(department_id):
     key_words = get_keywords(request.args.getlist("expand", None))
+    if invalid_keyword(key_words):
+        abort(404, 'check your keywords to expand')
     handler = BusinessResource()
     item = handler.get_item(department_id, Department.DATA)
     if key_words:
--- employee_resource/views.py
+++ employee_resource/views.py
@@ -2,6 +2,17 @@ from flask import jsonify, request
 from glide_api.app import app
 from glide_api.employee_resource.serializer import Employee
 from glide_api.utils import get_keywords
+from flask import abort
+
+
+def invalid_keyword(key_words):
+    valid_keys = ['department', 'superdepartment', 'manager', 'office']
+    for keys in key_words:
+        for key in keys:
+            if key in valid_keys:
+                continue
+            else:
+                return True
 
 
 @app.route("/employees", methods=["GET"])
@@ -10,6 +21,8 @@ def get_employee():
     limit = int(request.args.get("limit", "100"))
     expand_params = request.args.getlist('expand')
     key_words = get_keywords(expand_params)
+    if invalid_keyword(key_words):
+        abort(404, 'check your keywords to expand')
     employee = Employee()
     employees = employee.get_all(limit, offset, key_words)
     return jsonify(employees)
@@ -19,6 +32,8 @@ def get_employee():
 def get_employee_by_id(office_id):
     expand_params = request.args.getlist('expand', None)
     key_words = get_keywords(expand_params)
+    if invalid_keyword(key_words):
+        abort(404, 'check your keywords to expand')
     handler = Employee()
     if key_words:
         employee_data = handler.get_employee_by_id(office_id)
